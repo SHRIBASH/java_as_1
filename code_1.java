28e8560380e5b0b1671eaeb6a9cffc0e47a17d13
SHRIBASH <161469590+SHRIBASH@users.noreply.github.com>
2026-08-19 11:16:11 +0530

    Create code_1.java

---
+++ code_1.java
@@ -0,0 +1,18 @@
+ // Task 1. Write a program that declares variables for a student’s name, roll number, and marks in three
+ //         subjects. Assign values and print them. Ensure all variable names follow Java identifier rules.
+
+public class code_1 { 
+    public static void main (String[] args) {
+      String name = "Manan";
+      int roll_number = 06;
+      int statistics = 80;
+      int spanish = 90;
+      int java = 95;
+
+      System.out.println ("Student Name : " + name);
+      System.out.println ("Roll Number : " + roll_number);
+      System.out.println ("statistics : "  + statistics);
+      System.out.println ("spanish : " + spanish);
+      System.out.println ("java : " + java);
+    }
+}    
